e2f0576d6f449ca02e252b2f16958cd319098f92
Meiting <104717652+houmt-boreal@users.noreply.github.com>
2025-04-17 12:18:30 +0300

    Delete variates_Y.R

--- variates_Y.R
+++
@@ -1,8 +0,0 @@
-library(mixOmics)
-data(liver.toxicity)
-X <- liver.toxicity$gene
-Y <- liver.toxicity$clinic
-Y<-Y[1]
-toxicity.spls <- spls(X, Y, ncomp = 1)
-ex = explained_variance(toxicity.spls$Y, toxicity.spls$variates$Y, ncomp =1)
-ex2 = explained_variance(toxicity.spls$Y, toxicity.spls$variates$X, ncomp =1)
